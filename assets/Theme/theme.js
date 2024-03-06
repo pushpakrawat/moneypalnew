bdb9a23532b46cf4c5ed09a7ef604487b0fa9062
pushpakrawat <pushpak.rawat@outlook.com>
2024-03-06 13:59:01 +0530

    theme error fixed

--- assets/Theme/theme.js
+++ assets/Theme/theme.js
@@ -10,22 +10,31 @@ import { useSelector, useDispatch } from "react-redux";
 const Theme = () => {
   const [themeMode, setThemeMode] = useState(null);
   const [initialFetchDone, setInitialFetchDone] = useState(false);
-
-  // const userId = "VtxzLHQ2jc3aro8Dg2Fy";  
-  const userId = useSelector((state) => state.user.userId); //switch this when using in component
-
   const dispatch = useDispatch();
-
-  const themeColorRedux = useSelector((state) => state.app.themeColors);
-  console.log("ThemeColors fetched from redux:", themeColorRedux);
+  const userId = useSelector((state) => state.user.userId);
 
   const updateThemeColorsToRedux = (themeMode) => {
-    console.log("themeMode to update function: ", themeMode)
     const themeColors = themeMode === "light" ? light : dark;
-    console.log("themeColors to redux:", themeColors);
     dispatch(toggleThemeColors(themeColors));
+  };
 
-  }
+  const updateFirestore = async (newThemeMode) => {
+    try {
+      const userDocRef = doc(FIREBASE_DB, "users", userId);
+      await updateDoc(userDocRef, { themeMode: newThemeMode });
+    } catch (error) {
+      console.error("Error updating/fetching Firestore document:", error);
+    }
+  };
+
+  const toggleSwitch = () => {
+    if (initialFetchDone) {
+      const newThemeMode = themeMode === "light" ? "dark" : "light";
+      setThemeMode(newThemeMode);
+      updateThemeColorsToRedux(newThemeMode);
+      updateFirestore(newThemeMode);
+    }
+  };
 
   useEffect(() => {
     const fetchThemeMode = async () => {
@@ -35,10 +44,7 @@ const Theme = () => {
 
         if (userDocSnapshot.exists()) {
           const userData = userDocSnapshot.data();
-          console.log("Fetched theme from Firestore:", userData.themeMode);
           setThemeMode(userData.themeMode);
-          
-          //updating themeColors to redux
           updateThemeColorsToRedux(userData.themeMode);
         }
         setInitialFetchDone(true);
@@ -48,39 +54,9 @@ const Theme = () => {
     };
 
     fetchThemeMode();
-  }, [userId]); // Include userId as a dependency to refetch when it changes
-
-  const updateFirestore = async (newThemeMode) => {
-    try {
-      const userDocRef = doc(FIREBASE_DB, "users", userId);
-
-      // Update the existing document
-      await updateDoc(userDocRef, { themeMode: newThemeMode });
-      console.log("Updated Firestore with theme:", newThemeMode);
-    } catch (error) {
-      console.error("Error updating/fetching Firestore document:", error);
-    }
-  };
-
-  const toggleSwitch = () => {
-    // Use the callback version of setThemeMode to ensure up-to-date state
-    setThemeMode((prevThemeMode) => {
-      const newThemeMode = prevThemeMode === "light" ? "dark" : "light";
-      console.log("Toggled switch. New theme mode:", newThemeMode);
-      
-      updateThemeColorsToRedux(newThemeMode);
-
-      // Only update Firestore if initial fetch is complete
-      if (initialFetchDone) {
-        updateFirestore(newThemeMode);
-      }
-
-      return newThemeMode;
-    });
-  };
+  }, [userId]);
 
   if (themeMode === null) {
-    // Loading state or default value until the initial fetch is complete
     return (
       <View style={styles.container}>
         <Text>Loading...</Text>
@@ -108,19 +84,19 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "space-between",
     padding: 20,
-    // paddingVertical: 10,
   },
   textContainer: {
     flex: 1,
-    // marginBottom: 20,
   },
   themeText: {
     fontSize: 18,
-    // marginBottom: 20,
   },
 });
 
 export default Theme;
+
+
+
 
 
 // actionType ***********************************
